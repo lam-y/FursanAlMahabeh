2ffa529d2bfddce5764d8af3bedfbf987ef8dc1f
Lamis Olaiky <l.olaiky@syriatrust.sy>
2025-03-11 12:10:03 +0300

    fix circle image

--- resources/js/components/MembersListComponent.vue
+++ resources/js/components/MembersListComponent.vue
@@ -2,7 +2,7 @@
   <div class="row" dir="rtl">
         <div v-for="(member, index) in members.data" :key="index" class="col-lg-2">
             <div class="team-member">
-                <img class="mx-auto rounded-circle" :src="getImageUrl(member.photo)" alt="..." />
+                <img class="mx-auto circle-image" :src="getImageUrl(member.photo)" alt="..." />
                 <h4 class="small-title text-center name-font">{{ member.name }}</h4>
                 <p class="text-muted small-text text-center type-font">{{ member.member_type_name }}</p>
             </div>
