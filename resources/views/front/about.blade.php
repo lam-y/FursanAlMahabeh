a11662ec1858de28d5b9525fdeed686b887591c2
Lamis Olaiky <l.olaiky@syriatrust.sy>
2025-03-12 13:55:19 +0300

    fix jpg

--- resources/views/front/about.blade.php
+++ resources/views/front/about.blade.php
@@ -6,7 +6,7 @@
         </div>
         <ul class="timeline">
             <li>
-                <div class="timeline-image"><img class="rounded-circle img-fluid" src="{{asset('img/about/1.jpg')}}" alt="..." /></div>
+                <div class="timeline-image"><img class="rounded-circle img-fluid" src="{{asset('img/about/1.JPG')}}" alt="..." /></div>
                 <div class="timeline-panel">
                     <div class="timeline-heading">
                         <h4>1977-1978</h4>
@@ -16,7 +16,7 @@
                 </div>
             </li>
             <li class="timeline-inverted">
-                <div class="timeline-image"><img class="rounded-circle img-fluid" src="{{asset('img/about/2.jpg')}}" alt="..." /></div>
+                <div class="timeline-image"><img class="rounded-circle img-fluid" src="{{asset('img/about/2.JPG')}}" alt="..." /></div>
                 <div class="timeline-panel">
                     <div class="timeline-heading">
                         <h4>صيف عام 1978</h4>
@@ -26,7 +26,7 @@
                 </div>
             </li>
             <li>
-                <div class="timeline-image"><img class="rounded-circle img-fluid" src="{{asset('img/about/3.jpg')}}" alt="..." /></div>
+                <div class="timeline-image"><img class="rounded-circle img-fluid" src="{{asset('img/about/3.JPG')}}" alt="..." /></div>
                 <div class="timeline-panel">
                     <div class="timeline-heading">
                         <h4>1980-1981</h4>
@@ -36,16 +36,16 @@
                 </div>
             </li>
             <li class="timeline-inverted">
-                <div class="timeline-image"><img class="rounded-circle img-fluid" src="{{asset('img/about/4.jpg')}}" alt="..." /></div>
+                <div class="timeline-image"><img class="rounded-circle img-fluid" src="{{asset('img/about/4.JPG')}}" alt="..." /></div>
                 <div class="timeline-panel">
                     <div class="timeline-heading">
                         <h4>حتى اليوم</h4>
                         {{-- <h4 class="subheading">Phase Two Expansion</h4> --}}
                     </div>
-                    <div class="timeline-body"><p class="text-muted">مُنْذُ بِدَايَات الْفُرْقَة وَحَتَّى يَوْمِنَا هَذَا، بَقِيَ تَوَجُّهِهَا وَاضِحًا فِي بِنَاءِ إنْسَانٍ مُؤْمِن وَمَوَاطِن صَالِحٍ يَقُومُ بِرِسَالَتِه تُجَاه الْكَنِيسَة وَالْوَطَن الْإِنْسَان. و تَفْتَخِر فُرْقَة "فَرَسَان الْمَحَبَّة" بِأَنَّهَا وَعَلَى الرَّغْمِ مِنْ كُلِّ الصُّعُوبَات الَّتِي وَاجِهَتِهَا بَقِيَتْ أَمِينِه لِلأسس وَالمَبَادِئِ وَاَلَّتِي سَعَتْ وَتَسْعَى لِتَحْقِيقِهَا 
+                    <div class="timeline-body"><p class="text-muted">مُنْذُ بِدَايَات الْفُرْقَة وَحَتَّى يَوْمِنَا هَذَا، بَقِيَ تَوَجُّهِهَا وَاضِحًا فِي بِنَاءِ إنْسَانٍ مُؤْمِن وَمَوَاطِن صَالِحٍ يَقُومُ بِرِسَالَتِه تُجَاه الْكَنِيسَة وَالْوَطَن الْإِنْسَان. و تَفْتَخِر فُرْقَة "فَرَسَان الْمَحَبَّة" بِأَنَّهَا وَعَلَى الرَّغْمِ مِنْ كُلِّ الصُّعُوبَات الَّتِي وَاجِهَتِهَا بَقِيَتْ أَمِينِه لِلأسس وَالمَبَادِئِ وَاَلَّتِي سَعَتْ وَتَسْعَى لِتَحْقِيقِهَا
                         كَذَلِكَ تَتَمَيَّز فُرْقَة "فَرَسَان الْمَحَبَّة"، بِرَوَابِط قَوِيَّة تَجْمَع كَافَّة أَعْضَائِهَا مِنْ مُخْتَلَفِ الأَجْيَال..
                     <br>
-                    عَلَى الرَّغْمِ مِنْ اعْتِزَاز وَتَشَبَّث الْفُرْقَة بِنِظَامِهَا و تَقَالِيدِهَا وَ تَارِيخِهَا، فَإِنَّهَا تُحَاوِل دَائِمًا أَنْ تُقَدَّمَ مَا يُسَاهِمُ فِي بِنَاءِ أَعْضَائِهَا وَمَنْ حَوْلَهُمْ مَهْمَا تَغَيَّرَتْ الظُّرُوف و الأَجْيَال ... 
+                    عَلَى الرَّغْمِ مِنْ اعْتِزَاز وَتَشَبَّث الْفُرْقَة بِنِظَامِهَا و تَقَالِيدِهَا وَ تَارِيخِهَا، فَإِنَّهَا تُحَاوِل دَائِمًا أَنْ تُقَدَّمَ مَا يُسَاهِمُ فِي بِنَاءِ أَعْضَائِهَا وَمَنْ حَوْلَهُمْ مَهْمَا تَغَيَّرَتْ الظُّرُوف و الأَجْيَال ...
                     <br>
                     وَتَبْقَى "فَرَسَان الْمَحَبَّة".
                     </p></div>
@@ -56,7 +56,7 @@
                     <h4>
                         يسعدنا أن تكون
                         <br />
-                        عضواً في 
+                        عضواً في
                         <br />
                         فريقنا
                     </h4>
--- resources/views/front/goals.blade.php
+++ resources/views/front/goals.blade.php
@@ -7,21 +7,21 @@
         <div class="row text-center">
             <div class="col-md-4">
                 <span class="image-stack">
-                    <img src="{{asset('img/goals/goal-1.jpg')}}" alt="Icon" class="circle-image">
+                    <img src="{{asset('img/goals/goal-1.JPG')}}" alt="Icon" class="circle-image">
                 </span>
                 <h4 class="my-3">توجّه ثابت ومنظّم</h4>
                 <p class="text-muted">تعتمد الفرقة على مبادئ وقوانين واضحة تنظم عملها وفق نظام داخلي يحدد المسؤوليات والحقوق والتقييم، مع تطوير أساليب التقديم بما يتناسب مع المراحل المختلفة.</p>
             </div>
             <div class="col-md-4">
                 <span class="image-stack">
-                    <img src="{{asset('img/goals/goal-2.jpg')}}" alt="Icon" class="circle-image">
+                    <img src="{{asset('img/goals/goal-2.JPG')}}" alt="Icon" class="circle-image">
                 </span>
                 <h4 class="my-3">هويّة روحيّة وكشفيّة</h4>
                 <p class="text-muted">تستمد الفرقة مبادئها من تعاليم المسيح والكنيسة، مع التزامها بالروح الكشفية التي تعزز القيم الأخلاقية، حب الوطن، الاستعداد، والاعتماد على النفس.</p>
             </div>
             <div class="col-md-4">
                 <span class="image-stack">
-                    <img src="{{asset('img/goals/goal-3.jpg')}}" alt="Icon" class="circle-image">
+                    <img src="{{asset('img/goals/goal-3.JPG')}}" alt="Icon" class="circle-image">
                 </span>
                 <h4 class="my-3">دور تربوي شامل</h4>
                 <p class="text-muted">تساهم الفرقة في تنمية أعضائها جسديًا، فكريًا، وروحيًا، مكملةً دور الأهل في غرس المسؤولية تجاه العائلة، الوطن، ونقل رسالة الفرقة للمجتمع.</p>
